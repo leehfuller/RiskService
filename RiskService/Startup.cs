6da9088edf0448f4a18f9f9b9432de8187d29f8e
Lee Fuller <lee@fullerdata.com>
2021-11-15 12:11:47 -0500

    messing up azure deploys

--- RiskService/Startup.cs
+++ RiskService/Startup.cs
@@ -30,7 +30,7 @@ namespace RiskService
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "RiskService", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Risk Mission Service", Version = "v1" });
             });
         }
 
@@ -41,7 +41,7 @@ namespace RiskService
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RiskService v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Risk Mission Service"));
             }
 
             app.UseHttpsRedirection();
